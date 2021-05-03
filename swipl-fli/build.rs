cc94fbf0094e9701b3d93a3f5f7a176b2cd19b4c
Matthijs van Otterdijk <matthijs@terminusdb.com>
2021-05-03 15:28:59 +0200

    experimental static build support

--- swipl-fli/build.rs
+++ swipl-fli/build.rs
@@ -6,10 +6,23 @@ use swipl_info::*;
 fn main() {
     let info = get_swipl_info();
     if cfg!(target_os = "windows") {
-        println!("cargo:rustc-link-lib=libswipl");
+        if cfg!(feature = "static") {
+            println!("cargo:rustc-link-lib=static=libswipl_static");
+        } else {
+            println!("cargo:rustc-link-lib=dylib=libswipl");
+        }
         println!("cargo:rustc-link-search={}/bin", info.swi_home);
     } else {
-        println!("cargo:rustc-link-lib=swipl");
+        if cfg!(feature = "static") {
+            println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu/");
+            println!("cargo:rustc-link-lib=static=gmp");
+            println!("cargo:rustc-link-lib=static=curses");
+            println!("cargo:rustc-link-lib=static=tinfo");
+            println!("cargo:rustc-link-lib=static=z");
+            println!("cargo:rustc-link-lib=static=swipl_static");
+        } else {
+            println!("cargo:rustc-link-lib=dylib=swipl");
+        }
         println!(
             "cargo:rustc-link-search={}/{}",
             info.swi_home, info.pack_so_dir
