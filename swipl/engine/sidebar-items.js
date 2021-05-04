b1f22e11b11597e588960926ca114a2a3e284dc5
matko <matthijs@terminusdb.com>
2021-05-04 15:16:51 +0000

    Deploying to gh-pages from @ terminusdb-labs/swipl-rs@9c66300aaa596de8cb5f5b7679ebfa734f23a971 🚀

--- swipl/engine/sidebar-items.js
+++ swipl/engine/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["current_engine_ptr",""],["initialize_swipl",""],["initialize_swipl_noengine",""],["is_engine_active",""],["register_foreign_in_module",""]],"struct":[["Engine",""],["EngineActivation",""]]});
+initSidebarItems({"fn":[["current_engine_ptr","Returns the current engine pointer."],["is_engine_active","Checks if the given engine pointer is the engine that is currently active on this thread."]],"struct":[["Engine","A Prolog engine."],["EngineActivation","A prolog engine activation."]]});
