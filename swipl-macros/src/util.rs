c5cd057159dd7af3623981318083f5971e8647ef
Matthijs van Otterdijk <matthijs@terminusdb.com>
2021-05-05 23:00:07 +0200

    change the crate_token macro helper to detect doctest situation

--- swipl-macros/src/util.rs
+++ swipl-macros/src/util.rs
@@ -4,11 +4,27 @@ use quote::quote;
 use syn::Ident;
 
 pub fn crate_token() -> TokenStream {
-    match crate_name("swipl") {
-        Ok(name) => {
-            let name_ident = Ident::new(&name, Span::call_site());
-            quote! {#name_ident}
+    let current_crate_name = std::env::var("CARGO_CRATE_NAME");
+    if current_crate_name.is_ok() && current_crate_name.unwrap() == "swipl" {
+        // check that this is either compiling swipl as the primary package, or that environment variables indicating a rustdoc binary compile is missing.
+        if std::env::var("CARGO_PRIMARY_PACKAGE").is_ok() ||
+            std::env::var("UNSTABLE_RUSTDOC_TEST_LINE").is_err() {
+            quote!{crate}
         }
-        Err(_) => quote! {crate},
+        else {
+            // not the primary package but one of the tests. assume 'swipl'.
+            quote!{swipl}
+        }
+    }
+    else {
+        match crate_name("swipl") {
+            Ok(name) => {
+                let name_ident = Ident::new(&name, Span::call_site());
+                quote! {#name_ident}
+            }
+            Err(_) => {
+                quote! {swipl}
+            },
+        }
     }
 }
