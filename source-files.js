bd33dac5c401f4a83610011137eef5e98c3b0b77
matko <matthijs@terminusdb.com>
2021-05-05 08:38:18 +0000

    Deploying to gh-pages from @ terminusdb-labs/swipl-rs@fa67f5929f7b77ead1f553b116a9d1d4b7dfc9c3 🚀

--- source-files.js
+++ source-files.js
@@ -1,4 +1,5 @@
 var N = null;var sourcesIndex = {};
+sourcesIndex["standalone_example"] = {"name":"","files":["main.rs"]};
 sourcesIndex["swipl"] = {"name":"","files":["atom.rs","blob.rs","callable.rs","consts.rs","context.rs","engine.rs","fli.rs","functor.rs","init.rs","lib.rs","module.rs","predicate.rs","prelude.rs","result.rs","stream.rs","term.rs","text.rs"]};
 sourcesIndex["swipl_fli"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["swipl_info"] = {"name":"","files":["lib.rs"]};
