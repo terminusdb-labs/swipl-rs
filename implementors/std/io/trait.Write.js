f5d43afd338fdb66b3eed282642d5753ac8085a8
matko <matthijs@terminusdb.com>
2022-01-02 03:52:38 +0000

    Deploying to gh-pages from @ terminusdb-labs/swipl-rs@719bce8896281cc2d1432b5ecaf088a5b52f80c8 🚀

--- implementors/std/io/trait.Write.js
+++ implementors/std/io/trait.Write.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["swipl"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"swipl/stream/struct.PrologStream.html\" title=\"struct swipl::stream::PrologStream\">PrologStream</a>","synthetic":false,"types":["swipl::stream::PrologStream"]}];
+implementors["swipl"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.57.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"swipl/stream/struct.PrologStream.html\" title=\"struct swipl::stream::PrologStream\">PrologStream</a>","synthetic":false,"types":["swipl::stream::PrologStream"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
