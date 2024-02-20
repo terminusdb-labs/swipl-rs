61a54baf6c0447246e70f8a69bb826f1f9225772
matko <matthijs@terminusdb.com>
2024-02-20 09:28:18 +0000

    Deploying to gh-pages from @ terminusdb-labs/swipl-rs@20f0b622ef071e14d8117dff630310fd23cac46e 🚀

--- trait.impl/serde/de/trait.Deserialize.js
+++ trait.impl/serde/de/trait.Deserialize.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"swipl":[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.196/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"swipl/atom/struct.Atom.html\" title=\"struct swipl::atom::Atom\">Atom</a>"]]
+"swipl":[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.197/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"swipl/atom/struct.Atom.html\" title=\"struct swipl::atom::Atom\">Atom</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
